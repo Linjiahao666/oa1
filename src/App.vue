65e06aa593147cd2a642522626bfca99c1f71905
jcat <2195781154@qq.com>
2024-07-15 22:25:14 +0800

    feat#000:初始化工程

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main">
+    <base-navigator />
+  </div>
+</template>
+
+<script setup>
+import BaseNavigator from '@/components/navigator/BaseNavigator.vue';
+</script>
+
+<style scoped lang='scss'></style>
