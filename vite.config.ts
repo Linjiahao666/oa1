4a8bb97d4b2717dff2a963c8895347b33bb59a22
jcat <2195781154@qq.com>
2024-08-01 00:29:15 +0800

    feat(#000): 修改自动导入插件版本

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,24 @@ import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import Icons from 'unplugin-icons/vite'
+// import IconsResolver from 'unplugin-icons/resolver'
 import { viteMockServe } from 'vite-plugin-mock';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // ElementPlus({ resolver: [ElementPlusResolver()] }),
     AutoImport({
-      resolvers: [ElementPlusResolver()]
+      imports: ['vue'],
+      resolvers: [ElementPlusResolver(), /*IconsResolver({ prefix: 'Icon' })*/],
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [ElementPlusResolver(/*{ importStyle: 'sass' }*/), /*IconsResolver({ enabledCollections: ['ep'] })*/],
     }),
+    // Icons({
+    //   autoInstall: true,
+    // }),
     viteMockServe({
       mockPath: './src/mock', // mock 文件夹路径
       localEnabled: true, // 是否启用本地 mock
